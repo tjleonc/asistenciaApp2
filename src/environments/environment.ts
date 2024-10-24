0cf94e9bad46f2754d697bd08fab17b4fd5bbdc8
Seavuelo <SegaFlomer@gmail.com>
2024-10-24 03:34:29 -0300

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,29 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+import { initializeApp } from "firebase/app";
 export const environment = {
-  production: false
+
+  production: false,  // Añade esta propiedad
+
+  firebase: {
+    apiKey: "AIzaSyD5QN41VxhjbgwraVD9SZWEVKqQjfwHN3k",
+    authDomain: "ionic-asistencias.firebaseapp.com",
+    projectId: "ionic-asistencias",
+    storageBucket: "ionic-asistencias.appspot.com",
+    messagingSenderId: "447452325467",
+    appId: "1:447452325467:web:625cdfd821ed790342417e"
+  }
 };
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+
+// Initialize Firebase
+const app = initializeApp(environment.firebase);
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { initializeApp } from 'firebase/app'; // Importación directa de Firebase
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+// Inicialización de Firebase fuera del NgModule
+initializeApp(environment.firebase);
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.log(err));  
